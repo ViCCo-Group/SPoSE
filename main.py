e017892cf174922633437a73193c12658a846e76
Lukas Muttenthaler <lmuttenthaler@ltgr-hebart6.local>
2020-11-04 12:20:44 +0100

    removed vspose

--- main.py
+++ main.py
@@ -174,16 +174,16 @@ def run(
     lmbda /= n_items
 
     #initialize model and optimizer
-    if version == 'variational':
-        #variational version of SPoSE
-        model = VSPoSE(in_size=n_items, out_size=embed_dim)
-        k = 3 if task == 'odd_one_out' else 2
-        mu = torch.zeros(batch_size * k, embed_dim).to(device)
-        l = torch.ones(batch_size * k, embed_dim).mul(lmbda).to(device)
-        n_batches = len(train_batches) #for each mini-batch kld must be scaled by 1/B, where B = n_batches
-    else:
-        #deterministic version of SPoSE
-        model = SPoSE(in_size=n_items, out_size=embed_dim, init_weights=True)
+    #if version == 'variational':
+    #    #variational version of SPoSE
+    #    model = VSPoSE(in_size=n_items, out_size=embed_dim)
+    #    k = 3 if task == 'odd_one_out' else 2
+    #    mu = torch.zeros(batch_size * k, embed_dim).to(device)
+    #    l = torch.ones(batch_size * k, embed_dim).mul(lmbda).to(device)
+    #    n_batches = len(train_batches) #for each mini-batch kld must be scaled by 1/B, where B = n_batches
+    #else:
+    #deterministic version of SPoSE
+    model = SPoSE(in_size=n_items, out_size=embed_dim, init_weights=True)
 
     #initialise optimizer
     optim = Adam(model.parameters(), lr=lr)
@@ -244,22 +244,16 @@ def run(
             optim.zero_grad() #zero out gradients
             batch = batch.to(device)
 
-            if version == 'variational':
-                logits, z, mu_hat, l_hat = model(batch, device)
-            else:
-                logits = model(batch)
+            logits = model(batch)
 
             anchor, positive, negative = torch.unbind(torch.reshape(logits, (-1, 3, embed_dim)), dim=1)
             #TODO: figure out why the line below is necessary if we don't use the variable probs anywhere else in the script
             #probs = trinomial_probs(anchor, positive, negative, task)
             c_entropy = trinomial_loss(anchor, positive, negative, task)
 
-            if version == 'variational':
-                loss = c_entropy + (1/n_batches) * kl_div(mu_hat, l_hat, mu, l)
-            else:
-                l1_pen = l1_regularization(model).to(device) #L1-norm to enforce sparsity (many 0s)
-                pos_pen = torch.sum(F.relu(-model.fc.weight)) #positivity constraint to enforce non-negative values in our embedding matrix
-                loss = c_entropy + 0.01 * pos_pen + lmbda * l1_pen
+            l1_pen = l1_regularization(model).to(device) #L1-norm to enforce sparsity (many 0s)
+            pos_pen = torch.sum(F.relu(-model.fc.weight)) #positivity constraint to enforce non-negative values in our embedding matrix
+            loss = c_entropy + 0.01 * pos_pen + lmbda * l1_pen
 
             loss.backward() #backpropagate loss into the network
             optim.step() #compute gradients and update weights accordingly
@@ -283,10 +277,7 @@ def run(
             for j, batch in enumerate(val_batches):
                 batch = batch.to(device)
 
-                if version == 'variational':
-                    logits, _, _, _ = model(batch, device)
-                else:
-                    logits = model(batch)
+                logits = model(batch)
 
                 anchor, positive, negative = torch.unbind(torch.reshape(logits, (-1, 3, embed_dim)), dim=1)
                 val_loss = trinomial_loss(anchor, positive, negative, task)
--- models/model.py
+++ models/model.py
@@ -3,7 +3,6 @@
 
 __all__ = [
             'SPoSE',
-            'VSPoSE',
             'l1_regularization',
             ]
 
@@ -14,39 +13,6 @@ import torch.nn.functional as F
 
 from torch.distributions.laplace import Laplace
 from typing import Tuple
-
-class VSPoSE(nn.Module):
-
-    def __init__(
-                self,
-                in_size:int,
-                out_size:int,
-                eps:float=1e-7,
-                ):
-        super(VSPoSE, self).__init__()
-        self.in_size = in_size
-        self.out_size = out_size
-        self.eps = eps
-        #NOTE: in the end we want to extract the model parameters W_mu and W_b to sample objects offline
-        self.encoder_mu = nn.Linear(self.in_size, self.out_size, bias=True)
-        self.encoder_b = nn.Linear(self.in_size, self.out_size, bias=True)
-        self.decoder = nn.Linear(self.out_size, self.out_size, bias=True)
-
-    def reparameterize(self, mu:torch.Tensor, b:torch.Tensor, device:torch.device) -> torch.Tensor:
-        laplace = Laplace(loc=torch.zeros(b.size()), scale=torch.ones(b.size()))
-        U = laplace.sample().to(device) #draw random sample from a standard Laplace distribution with mu = 0 and lam = 1
-        z = U.mul(b).add(mu) #perform reparameterization trick
-        z = z.abs() #apply absolute value to impose non-negativity constraint on z
-        return z
-
-    def forward(self, x:torch.Tensor, device:torch.device) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        mu = self.encoder_mu(x) #mu = mode = loc param
-        b = self.encoder_b(x) #b = scale param
-        b = b.clamp(min=self.eps) #b is constrained to be in the positive real number space R+ (b > 0)
-        z = self.reparameterize(mu, b, device)
-        logits = self.decoder(z)
-        l = (b.log()*-1.0).exp() #this is equivalent to but numerically more stable than b.pow(-1)
-        return logits, z, mu, l
 
 class SPoSE(nn.Module):
 
