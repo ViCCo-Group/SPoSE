d6a340b10edd31fce300e245dbd85fe21bcffbdc
LukasMut <muttenthaler@cbs.mpg.de>
2021-06-02 15:21:39 +0200

    added two different versions of nmf

--- models/nmf.py
+++ models/nmf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__all__ = ['NeuralNMF', 'BatchNMF']
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,7 +10,37 @@ import numpy as np
 
 from typing import Tuple
 
-class NNMF(nn.Module):
+class NeuralNMF(nn.Module):
+
+    def __init__(
+                self,
+                n_samples:int,
+                n_components:int,
+                n_features:int,
+                init_weights:bool,
+                W=None,
+                H=None,
+    ):
+    super(NeuralNMF, self).__init__()
+    self.n_samples = n_samples
+    self.n_components = n_components
+    self.n_features = n_features
+    self.W = nn.Linear(self.n_samples, self.n_components, bias=False)
+
+    if init_weights:
+        assert isinstance(W, np.ndarray), '\nTo initialise components with W, NMF solution must be provided'
+        assert isinstance(H, np.ndarray), 'To initialise coefficients with H, NMF solution must be provided\n'
+        self.W.weight.data = torch.from_numpy(W.T)
+        self.H = nn.Parameter(torch.from_numpy(H), requires_grad=True)
+    else:
+        self.H = nn.Parameter(torch.randn(self.n_features, self.n_components), requires_grad=True)
+
+    def forward(self, x:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        logits = x @ self.W.weight.T.abs()
+        X_hat = self.W.weight.T.abs() @ self.H.T.abs()
+        return X_hat, logits
+
+class BatchNMF(nn.Module):
 
     def __init__(
                 self,
@@ -35,5 +67,5 @@ class NNMF(nn.Module):
 
     def forward(self, x:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         X_hat = self.W.abs() @ self.H.T.abs()
-        logits = x @ X_hat
+        logits = x @ self.W.abs()
         return X_hat, logits
