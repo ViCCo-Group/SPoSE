aca438a0cff15c34a0510b03cb55e01ecc4fe547
LukasMut <muttenthaler@cbs.mpg.de>
2021-06-08 14:47:16 +0200

    added nmf models

--- models/nmf.py
+++ models/nmf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__all__ = ['NeuralNMF', 'BatchNMF']
+__all__ = ['NeuralNMF__', 'NeuralNMF_', 'NeuralNMF', 'BatchNMF']
 
 import torch
 import torch.nn as nn
@@ -9,6 +9,59 @@ import torch.nn.functional as F
 import numpy as np
 
 from typing import Tuple
+
+
+class NeuralNMF__(nn.Module):
+
+    def __init__(
+                self,
+                n_samples:int,
+                n_components:int,
+                n_features:int,
+                W:np.ndarray,
+                H:np.ndarray=None,
+    ):
+        super(NeuralNMF__, self).__init__()
+        self.n_samples = n_samples
+        self.n_components = n_components
+        self.n_features = n_features
+        self.W = nn.Linear(self.n_samples, self.n_components, bias=False)
+        self.W.weight.data = torch.from_numpy(W.T).type(self.W.weight.data.dtype)
+        self.W.weight.requires_grad = False
+        self.H = torch.from_numpy(H.T).type(self.W.weight.data.dtype)
+
+        self.a = nn.Parameter(torch.randn(self.n_components), requires_grad=True)
+
+    def forward(self, x:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        logits = self.W(x) * self.a.abs().sqrt()
+        return logits
+
+class NeuralNMF_(nn.Module):
+
+    def __init__(
+                self,
+                n_samples:int,
+                n_components:int,
+                n_features:int,
+                W:np.ndarray,
+                H:np.ndarray=None,
+    ):
+        super(NeuralNMF_, self).__init__()
+        self.n_samples = n_samples
+        self.n_components = n_components
+        self.n_features = n_features
+        self.W = nn.Linear(self.n_samples, self.n_components, bias=False)
+        self.W.weight.data = torch.from_numpy(W.T).type(self.W.weight.data.dtype)
+        self.W.weight.requires_grad = False
+        self.H = torch.from_numpy(H.T).type(self.W.weight.data.dtype)
+
+        self.A = nn.Linear(self.n_components, self.n_components, bias=False)
+
+    def forward(self, x:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        self.A.weight.data.abs_()
+        z = self.W(x)
+        logits = self.A(z)
+        return logits
 
 class NeuralNMF(nn.Module):
 
